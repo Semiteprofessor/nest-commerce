cdb434d2e449d6ca1dbe123235817830c907e0ee
Taiwo Olapade <44630802+Semiteprofessor@users.noreply.github.com>
2025-02-08 18:39:53 +0100

    Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/db-source';
+import { TestModule } from './test/test.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions)],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), TestModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
