60dba02a3b7adfb5c19d18333b93fa0e8568331c
Taiwo Olapade <44630802+Semiteprofessor@users.noreply.github.com>
2025-02-16 15:22:23 +0100

    Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from './db/db-source';
 import { UsersModule } from './users/users.module';
+import { LoggerMiddleware } from './utiltiy/middleware/current-user.midleware';
 
 @Module({
   imports: [TypeOrmModule.forRoot(dataSourceOptions), UsersModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
